ff546897ba418e6731d1a94ba307c2095baf739e
Joyboy97 <winnerstar29@gmail.com>
2022-11-10 11:54:36 -0500

    packagelock fix???

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,6 @@ function App(): JSX.Element {
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
             </p>
-
         </div>
     );
 }
